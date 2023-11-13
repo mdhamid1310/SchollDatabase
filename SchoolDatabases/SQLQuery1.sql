3580965d7ae5f4649f169609c89fe1d462767cd9
mdhamid1310 <john@example.com>
2023-11-13 09:24:19 +0530

    Add project files.

---
+++ SchoolDatabases/SQLQuery1.sql
@@ -0,0 +1,28 @@
+﻿-- Create Student table
+CREATE TABLE Student (
+    StudentID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    DateOfBirth DATE,
+    Gender CHAR(1),
+    ClassID INT, -- Foreign key referencing Classes table
+    CONSTRAINT FK_Student_Class FOREIGN KEY (ClassID) REFERENCES Classes(ClassID)
+);
+
+-- Create Subjects table
+CREATE TABLE Subjects (
+    SubjectID INT PRIMARY KEY,
+    SubjectName VARCHAR(50)
+);
+
+-- Create Classes table
+CREATE TABLE Classes (
+    ClassID INT PRIMARY KEY,
+    ClassName VARCHAR(50)
+);
+
+sp_help Student
+
+sp_help Subjects
+
+sp_help Classes
